c5c8495afc3b46dc0eaa91d4c67304ed463027a2
OccamMDCEO <gilesfc@me.com>
2013-03-05 21:14:38 -0500

    it works

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,106 @@
+class ClientsController < ApplicationController
+  
+
+  # GET /clients
+  # GET /clients.json
+  def index
+    @clients = Client.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @clients }
+    end
+
+  end
+
+
+  def send_text_message
+    @client = Client.find(params[:id])
+    number_to_send_to = @client.phone
+ 
+    twilio_sid = 'ACee185e4fdf1ff6dac321d90841f23f25'
+    twilio_token = '6fcd44041f413e6db99c8513673e0a5a'
+    twilio_phone_number = "6479311279"
+ 
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+ 
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => @client.message
+    )
+  end
+
+
+
+  # GET /clients/1
+  # GET /clients/1.json
+  def show
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @client }
+    end
+  end
+
+  # GET /clients/new
+  # GET /clients/new.json
+  def new
+    @client = Client.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @client }
+    end
+  end
+
+  # GET /clients/1/edit
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  # POST /clients
+  # POST /clients.json
+  def create
+    @client = Client.new(params[:client])
+
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.json { render json: @client, status: :created, location: @client }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /clients/1
+  # PUT /clients/1.json
+  def update
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      if @client.update_attributes(params[:client])
+        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @client.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /clients/1
+  # DELETE /clients/1.json
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+
+    respond_to do |format|
+      format.html { redirect_to clients_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilio2::Application.config.secret_token = '36f71121f5fb4e272f2471f8ad03ab92b4b13a70f8f6f243cc3a72e6563fe31464d56cab53707a0a81449018522b806ecaf1e07888440b2262f318f7ec8bda94'
